43a2bb10a7f3410e7ca0db511106033e69cebcc3
ruangmas <ruangmas@umd.edu>
2024-02-22 15:44:26 -0500

    make raster from coordinates

--- read_Berman_data.R
+++ read_Berman_data.R
@@ -1,5 +1,23 @@
 library("haven")
+library("tidyverse")
 
-dir("Berman/Data/")
-df<-read_dta("Berman/Data/BCRT_baseline.dta")
+dir("20150774_data/Data/")
+df<-read_dta("20150774_data/Data/BCRT_baseline.dta")
 names(df)
+
+lon<-unique(df$longitude)
+lon<-subset(lon, lon>0)
+lat<-unique(df$latitude) 
+
+coord<-crossing(lon, lat)
+
+library("terra")
+
+x <- vect(coord, geom=c("lon", "lat"),
+          crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ")
+r<-rast(x)
+plot(r)
+
+library("tmap")
+tm_shape(r)+
+  tm_raster(col="red")
